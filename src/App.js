efebf10d795a91f1c0b2f059419ab195aa4ff865
Aniket <vishnoianiket4@gmail.com>
2022-12-28 23:25:32 +0530

    react-router Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   
   return (
     <>
+    <Router>
     <div>
-     <Textform heading = "Enter the text here"/>
+      <Navbar/>
     </div>
+    <Routes>
+          <Route element={<About/>} exact path="/about"/>
+          <Route element={<Textform heading = "Enter the text here"/>} exact path="/"/> 
+        </Routes>
+    <div>
+    </div>
+    </Router>
+
     </>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>Here is the about section</h1>
+    </div>
+  )
+}
